762b2c43cd82ccd97745685c906a389ffa881a37
Erick <57086385+Erick-Rick22@users.noreply.github.com>
2022-12-08 18:06:00 -0600

    Add files via upload

---
+++ Simple.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.7;
+
+contract Simple {
+    
+    string public myCity = "Mexico";
+	string public myProp = "Casa";
+	string public myRegPub = "279072";
+	string public myValNot = "138";
+	string public myNumCont = "C-1526";
+	string public myFecha = "08 - Diciembre - 2022";
+	string public myCaract = "Propiedad de 200 m con 3 dormitorios, 2 baños, salón comedor, cocina y terraza. Zona tranquila, amplitud y belleza, buena entrada de luz natural. Ubicada en Ciudad de México ";
+
+}
